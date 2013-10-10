d3fa379838789868ff97fff7652e3538b30ec796
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2013-10-10 22:19:37 +0200

    save points as hashmap & AnimationPostproduction
    As planned in #8 i got rid of the clunky searchPointByID help-method
    through using a HashMap with the node/point IDs as keys.
    
    Giving weight to the conceptual element of caring about the
    layouting-animation AFTER the actual layout-computation has been done.
    This was present in the radialPlainLayout class before but giving it an
    extra-class elevates it's conceptual status and may pave the wave for
    adding other "postProduction" layouting like a strict left-to-right one
    for instance or a depth-first kind of one that starts from the branch on
    the very left and works its way to the right. Or starting in the middle
    and working towards both sides simultaneously? Cool things possible i'd
    imagine.
    Not sure yet if it would be better as a static class or by implementing
    an interface like Sanjo did with the layout algorithms...

---
+++ GraphVisualizer/src/model/AnimationPostproduction.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class AnimationPostproduction { 
+
+	/* could have also named it AnimationFaker? but that doesn't sound so nice :D
+	 * basically it's about faking the animation (for levelwise and nodewise, but later also other modes like horizontal or recursive-logic)
+	 * animation AFTER the actual calculation was done in the nodes themselves
+	 */
+
+	public AnimationPostproduction(){}
+	
+	public Graph createNodeAndLevelwiseBasedOnLevels(Graph graph){	
+		Map<String, Point> savingNodesPosHashMap = new HashMap<>();
+		
+		//saving the positions
+		for(Node node : graph.getNodes()){
+			savingNodesPosHashMap.put(node.getID(), new Point(node.getPoint()));
+			node.setPos(0, 0);
+		}
+		//and putting them back into the nodes, but in a certain order and with placing keyframes at each step
+		for(ArrayList<Node> level : graph.getNodesLevels()){
+			for(Node node : level){
+				Point savingPointForThisNode = savingNodesPosHashMap.get(node.getID());		
+				graph.nodeSetPos(node, savingPointForThisNode.x, savingPointForThisNode.y);
+			}
+			if(level.get(0) != graph.getRootnode()) //would add an unecessary keyframe for placing the rootnode at 0,0 which already is at 0,0
+				graph.addKeyframeLevelwise();
+		}		
+		return graph;
+	}
+	
+}
--- GraphVisualizer/src/model/RadialPlainLayout.java
+++ GraphVisualizer/src/model/RadialPlainLayout.java
@@ -1,11 +1,7 @@
 package model;
 
-import java.util.ArrayList;
-
 public class RadialPlainLayout implements LayoutInterface {
 	
-	ArrayList<Point> savingNodesPos;
-
 	@Override
 	public Graph performLayout(Graph graph, int nodeSize, int nodeMinHorizDist, int nodeVertDist) {				
 		Node rootnode = graph.getRootnode();
@@ -13,36 +9,9 @@ public class RadialPlainLayout implements LayoutInterface {
 		rootnode.setCorridor(0, 360);
 		rootnode.placingForRadialPlain(nodeVertDist + nodeSize); //decentralized... sending an impulse into rootnode for it to pass it downwards. awesome strategy... apply elsewhere? everywhere??
 
-		//faking the animation for levelwise and nodewise animation AFTER the actual calculation was done in the nodes themselves :) why? because otherwise Node would need to know Graph
-		//in order to do the "snapshots" (=new Keyframe for levewise or nodewise) and that doesn't feel elegant at all
-		savingNodesPos = new ArrayList<>();
-		for(Node node : graph.getNodes()){
-			savingNodesPos.add(new Point(node.getPoint()));
-			node.setPos(0, 0);
-		}
-				
-		for(ArrayList<Node> level : graph.getNodesLevels()){
-			for(Node node : level){
-				Point savingPointForThisNode = findPointByID(node.getID());
-				graph.nodeSetPos(node, savingPointForThisNode.x, savingPointForThisNode.y);
-				//node.putMyChildrenAtMyPos();
-			}
-			if(level.get(0) != graph.getRootnode()) //would add an unecessary keyframe for placing the rootnode at 0,0 which already is at 0,0
-				graph.addKeyframeLevelwise();
-		}
-
-		return graph;
+		return new AnimationPostproduction().createNodeAndLevelwiseBasedOnLevels(graph);
 	}
-
-	private Point findPointByID(String nodeID){
-		for(Point point : savingNodesPos)
-			if(point.nodeID.equals(nodeID))
-				return point;
-		return null;
-	}
-
 }
-
 
 
 
@@ -51,4 +20,11 @@ public class RadialPlainLayout implements LayoutInterface {
 //	child.setArc(node.getArc() / node.getChildren().size());
 //	setArcsRecursively(child);
 //}
+//}
+//
+//private Point findPointByID(String nodeID){
+//	for(Point point : savingNodesPos)
+//		if(point.nodeID.equals(nodeID))
+//			return point;
+//	return null;
 //}
